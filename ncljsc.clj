d6ffe25f5a6eeb64198cb1ae8dae1d675cf68fe3
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-04 10:29:53 -0500

    trying to fix exception related to reading files from disk (I think)

--- ncljsc.clj
+++ ncljsc.clj
@@ -1,6 +1,16 @@
 (ns ncljsc
   (:require [cljs.closure :as closure]))
 
+(def comp-options {})
+(def deps-options {})
+(def optm-options {:optimizations :advanced :pretty-print true})
+
 (defn build
   [source]
-  (closure/-compile source (closure/make-options {:optimizations :advanced})))
+  ;;(closure/optimize optm-options (closure/add-dependencies
+  ;;deps-options (closure/-compile source comp-options)))
+  ;;(closure/add-dependencies deps-options (closure/-compile source
+  ;comp-options))
+  (closure/add-dependencies {} "goog.provide('test.app');\ngoog.require('cljs.core');")
+  )
+  ;;(closure/-compile source comp-options)
