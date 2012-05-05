700d6a7a8a8fb44c78a22f06c7856beb58526e58
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-05 12:20:23 -0500

    more sensible postinstall script setup

---
+++ scripts/postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ./support
+rm -rf clojure-clojurescript-7472ab9
+rm -rf clojure-clojurescript
+tar xzf ./archive/clojure-clojurescript-r1011-0-g7472ab9.tar.gz
+mv clojure-clojurescript-7472ab9 clojure-clojurescript
+cp ./script/compile.sh ./clojure-clojurescript/script/compile
+cp ./cljs/nodejs.cljs ./clojure-clojurescript/src/cljs/cljs/nodejs.cljs
+cp ./cljs/nodejs_externs.js ./clojure-clojurescript/src/cljs/cljs/nodejs_externs.js
+cp ./cljs/nodejscli.cljs ./clojure-clojurescript/src/cljs/cljs/nodejscli.cljs
+cd ./clojure-clojurescript/ && ./script/bootstrap && ./script/compile
