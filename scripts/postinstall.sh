f5b31a8f8bd8d40a58709c3fa7ecf98cd71beef3
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-05 17:57:49 -0500

    improved postinstall script -- doesn't run if support/clojure-clojurescript exists, or if it does exist it runs if npm-debug.log exists in the package's root

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [ \( ! -e ./support/clojure-clojurescript \) -o \( -e ./npm-debug.log \) ]
+then
+
 cd ./support
 rm -rf clojure-clojurescript-7472ab9
 rm -rf clojure-clojurescript
@@ -9,4 +12,9 @@ cp ./script/compile.sh ./clojure-clojurescript/script/compile
 cp ./cljs/nodejs.cljs ./clojure-clojurescript/src/cljs/cljs/nodejs.cljs
 cp ./cljs/nodejs_externs.js ./clojure-clojurescript/src/cljs/cljs/nodejs_externs.js
 cp ./cljs/nodejscli.cljs ./clojure-clojurescript/src/cljs/cljs/nodejscli.cljs
-cd ./clojure-clojurescript/ && ./script/bootstrap && ./script/compile
+cd ./clojure-clojurescript/
+./script/bootstrap
+echo "Compiling..."
+./script/compile > /dev/null 2>&1
+
+fi
