f887fb786170dc9d16e2451b478715ea6f3d5f1c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-08 01:25:04 -0500

    bumping master to v0.1.0-pre

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*//////////////////////// NODE-CLOJURESCRIPT //////////////////////////
  *
- * Version 0.0.0-8
+ * Version 0.1.0-pre
  * https://github.com/michaelsbradleyjr/node-clojurescript
  *
  * This software is Copyright (c) 2012 by Michael Bradley, Jr.
@@ -48,7 +48,7 @@
   
   ClojureScript = {};
   
-  ClojureScript.VERSION = '0.0.0-8';
+  ClojureScript.VERSION = '0.1.0-pre';
   
   ClojureScript.CLJS_VERSION = 'r1011';
   
--- lib/clojure-script.coffee
+++ lib/clojure-script.coffee
@@ -13,7 +13,7 @@ vm             = require 'vm'
 
 ClojureScript = {}
 
-ClojureScript.VERSION = '0.0.0-8'
+ClojureScript.VERSION = '0.1.0-pre'
 
 ClojureScript.CLJS_VERSION = 'r1011'
 
