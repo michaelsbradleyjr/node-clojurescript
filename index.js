6d7f1f91779a5b148840dcb60a90d66e1c67e68d
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-09 01:31:04 -0500

    releasing v0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*//////////////////////// NODE-CLOJURESCRIPT //////////////////////////
  *
- * Version 0.1.0-pre
+ * Version 0.1.0
  * https://github.com/michaelsbradleyjr/node-clojurescript
  *
  * This software is Copyright (c) 2012 by Michael Bradley, Jr.
@@ -50,7 +50,7 @@
   
   ClojureScript = {};
   
-  ClojureScript.VERSION = '0.1.0-pre';
+  ClojureScript.VERSION = '0.1.0';
   
   ClojureScript.CLJS_VERSION = 'r1011';
   
